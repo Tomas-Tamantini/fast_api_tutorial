3bbe36451d7b5633cfb026c887ac1df94ba4f199
Tomas Tamantini <tomasbft@gmail.com>
2024-06-23 21:44:59 -0300

    Exercise class 2: HTML endpoint

--- fast_api_tutorial/app.py
+++ fast_api_tutorial/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,18 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.get("/html", response_class=HTMLResponse)
+def get_html():
+    return """
+    <html>
+        <head>
+            <title>hello world</title>
+        </head>
+        <body>
+            <h1>Hello world!</h1>
+            <p>In HTML format :)</p>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,10 @@ def test_read_root_returns_ok():
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello World"}
+
+
+def test_get_html_returns_ok():
+    client = TestClient(app)
+    response = client.get("/html")
+    assert response.status_code == HTTPStatus.OK
+    assert "Hello world!" in response.text
