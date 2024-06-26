270cd74193fd6385e6136ec494eb7772f41b4a07
Tomas Tamantini <tomasbft@gmail.com>
2024-06-26 16:39:05 -0300

    Inject fake user repository in unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,25 @@
 import pytest
+from unittest.mock import MagicMock
 from fastapi.testclient import TestClient
-from fast_api_tutorial.app import app
+from fast_api_tutorial.app import app, get_unit_of_work
+from fast_api_tutorial.persistence.user_repository import UserRepository
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def user_repository():
+    yield MagicMock(spec=UserRepository)
+
+
+@pytest.fixture
+def unit_of_work(user_repository):
+    with MagicMock() as mock:
+        mock.user_repository = user_repository
+        yield mock
+
+
+@pytest.fixture
+def client(unit_of_work):
+    with TestClient(app) as client:
+        app.dependency_overrides[get_unit_of_work] = lambda: unit_of_work
+        yield client
+        app.dependency_overrides.clear()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from fast_api_tutorial.exceptions import NotFoundException
 
 
 def test_read_root_returns_ok(client):
@@ -18,17 +19,20 @@ def test_create_invalid_user_returns_unprocessable_entity(client):
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
-def test_create_valid_user_returns_created(client):
+def test_create_valid_user_returns_created(client, user_repository):
+    user_repository.create.return_value = _user_response()
     response = client.post("/users/", json=_create_user_request(is_valid=True))
     assert response.status_code == HTTPStatus.CREATED
 
 
-def test_create_valid_user_response_does_not_return_password(client):
+def test_create_valid_user_response_does_not_return_password(client, user_repository):
+    mock_user = _user_response(password="123")
+    user_repository.create.return_value = mock_user
     request = _create_user_request(is_valid=True)
     response = client.post("/users/", json=request).json()
     assert "password" not in response
-    assert response["username"] == request["username"]
-    assert response["email"] == request["email"]
+    assert response["username"] == mock_user["username"]
+    assert response["email"] == mock_user["email"]
 
 
 def test_get_users_returns_ok(client):
@@ -36,100 +40,75 @@ def test_get_users_returns_ok(client):
     assert response.status_code == HTTPStatus.OK
 
 
-def test_get_users_returns_all_users(client):
-    client.post(
-        "/users/", json=_create_user_request(is_valid=True)
-    )  # TODO: Remove this line by injecting mock DB
+def test_get_users_returns_all_users(client, user_repository):
+    user_repository.get_all.return_value = [
+        _user_response(id=123),
+        _user_response(id=321),
+    ]
     response = client.get("/users/")
     assert "users" in response.json()
     users = response.json()["users"]
-    assert len(users) > 0
-    assert all("id" in user for user in users)
-    assert all("username" in user for user in users)
-    assert all("email" in user for user in users)
-    assert all("password" not in user for user in users)
+    assert [user["id"] for user in users] == [123, 321]
+    assert not any("password" in user for user in users)
 
 
-def test_get_user_returns_ok(client):
-    user = client.post(
-        "/users/", json=_create_user_request(is_valid=True)
-    )  # TODO: Remove this line by injecting mock DB
-    user_id = user.json()["id"]
-    response = client.get(f"/users/{user_id}/")
+def test_get_user_returns_ok(client, user_repository):
+    user_repository.get.return_value = _user_response()
+    response = client.get(f"/users/1/")
     assert response.status_code == HTTPStatus.OK
 
 
-def test_get_user_returns_user(client):
-    user = client.post(
-        "/users/", json=_create_user_request(is_valid=True)
-    )  # TODO: Remove this line by injecting mock DB
-    user_id = user.json()["id"]
-    response = client.get(f"/users/{user_id}/")
-    assert response.json()["id"] == user_id
-    assert response.json()["username"] == user.json()["username"]
-    assert response.json()["email"] == user.json()["email"]
-    assert "passwordf" not in response.json()
+def test_get_user_returns_user(client, user_repository):
+    mock_user = _user_response()
+    user_repository.get.return_value = mock_user
+    response = client.get(f"/users/1/")
+    assert response.json()["id"] == mock_user["id"]
+    assert response.json()["username"] == mock_user["username"]
+    assert response.json()["email"] == mock_user["email"]
+    assert "password" not in response.json()
 
 
-def test_get_non_existing_user_returns_not_found(client):
+def test_get_non_existing_user_returns_not_found(client, user_repository):
+    user_repository.get.side_effect = NotFoundException
     bad_id = _non_existing_id(client)
     response = client.get(f"/users/{bad_id}/")
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_update_existing_user_returns_ok(client):
-    user = client.post(
-        "/users/", json=_create_user_request(is_valid=True)
-    )  # TODO: Remove this line by injecting mock DB
-    user_id = user.json()["id"]
-    response = client.put(
-        f"/users/{user_id}/", json=_create_user_request(is_valid=True)
-    )
+def test_update_existing_user_returns_ok(client, user_repository):
+    user_repository.update.return_value = _user_response()
+    response = client.put(f"/users/1/", json=_create_user_request(is_valid=True))
     assert response.status_code == HTTPStatus.OK
 
 
-def test_update_existing_user_returns_updated_user(client):
-    user = client.post(
-        "/users/", json=_create_user_request(is_valid=True)
-    )  # TODO: Remove this line by injecting mock DB
-    user_id = user.json()["id"]
-    response = client.put(
-        f"/users/{user_id}/",
-        json={"username": "new", "email": "valid@valid.com", "password": "123"},
-    )
-    assert response.json()["username"] == "new"
-    assert response.json()["id"] == user_id
+def test_update_existing_user_returns_updated_user(client, user_repository):
+    mock_user = _user_response()
+    user_repository.update.return_value = mock_user
+    response = client.put(f"/users/1/", json=_create_user_request(is_valid=True))
+    assert response.json()["username"] == mock_user["username"]
+    assert response.json()["id"] == mock_user["id"]
 
 
 def test_invalid_user_update_returns_unprocessable_entity(client):
-    user = client.post(
-        "/users/", json=_create_user_request(is_valid=True)
-    )  # TODO: Remove this line by injecting mock DB
-    user_id = user.json()["id"]
-    response = client.put(
-        f"/users/{user_id}/", json=_create_user_request(is_valid=False)
-    )
+    response = client.put(f"/users/1/", json=_create_user_request(is_valid=False))
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
-def test_update_non_existing_user_returns_not_found(client):
-    bad_id = _non_existing_id(client)
-    response = client.put(f"/users/{bad_id}/", json=_create_user_request(is_valid=True))
+def test_update_non_existing_user_returns_not_found(client, user_repository):
+    user_repository.update.side_effect = NotFoundException
+    response = client.put(f"/users/123/", json=_create_user_request(is_valid=True))
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_delete_existing_user_returns_no_content(client):
-    user = client.post(
-        "/users/", json=_create_user_request(is_valid=True)
-    )  # TODO: Remove this line by injecting mock DB
-    user_id = user.json()["id"]
-    response = client.delete(f"/users/{user_id}/")
+def test_delete_existing_user_returns_no_content(client, user_repository):
+    user_repository.delete.return_value = None
+    response = client.delete(f"/users/1/")
     assert response.status_code == HTTPStatus.NO_CONTENT
 
 
-def test_delete_non_existing_user_returns_not_found(client):
-    bad_id = _non_existing_id(client)
-    response = client.delete(f"/users/{bad_id}/")
+def test_delete_non_existing_user_returns_not_found(client, user_repository):
+    user_repository.delete.side_effect = NotFoundException
+    response = client.delete(f"/users/123/")
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
@@ -139,6 +118,12 @@ def _create_user_request(is_valid: bool) -> dict:
         "email": "valid@valid.com" if is_valid else "invalid email",
         "password": "password",
     }
+
+
+def _user_response(
+    id: int = 1, username: str = "test", email: str = "a@b.com", password: str = "123"
+) -> dict:
+    return {"id": id, "username": username, "email": email, "password": password}
 
 
 def _non_existing_id(client):
