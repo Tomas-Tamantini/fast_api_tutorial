40caa6e274ac160f5ae9fb10e98417b2f12b4654
Tomas Tamantini <tomasbft@gmail.com>
2024-06-26 19:10:25 -0300

    Rename db tests

--- tests/test_persistence/test_relational_user_repository.py
+++ tests/test_persistence/test_relational_user_repository.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy import select
 from fast_api_tutorial.persistence.relational import RelationalUserRepository, User
-from fast_api_tutorial.exceptions import NotFoundException
+from fast_api_tutorial.exceptions import NotFoundException, DuplicateException
 
 
 @pytest.mark.integration
@@ -16,14 +16,14 @@ def test_create_user_saves_it_to_db(session, user_request):
 
 
 @pytest.mark.integration
-def test_getting_inexistent_user_raises_not_found_error(session):
+def test_getting_user_not_in_db_raises_not_found_error(session):
     repository = RelationalUserRepository(session)
     with pytest.raises(NotFoundException):
         repository.get_from_email("a@b.com")
 
 
 @pytest.mark.integration
-def test_getting_user_from_valid_email_returns_user(session, user_request):
+def test_getting_user_from_db_with_valid_email_returns_user(session, user_request):
     repository = RelationalUserRepository(session)
     repository.add(user_request(username="test", email="a@b.com"))
     session.commit()
@@ -32,14 +32,14 @@ def test_getting_user_from_valid_email_returns_user(session, user_request):
 
 
 @pytest.mark.integration
-def test_getting_inexistent_user_by_id_raises_not_found_error(session):
+def test_getting_user_by_id_not_in_db_raises_not_found_error(session):
     repository = RelationalUserRepository(session)
     with pytest.raises(NotFoundException):
         repository.get(id=1)
 
 
 @pytest.mark.integration
-def test_getting_user_by_id_returns_user(session, user_request):
+def test_getting_user_by_id_in_db_returns_user(session, user_request):
     repository = RelationalUserRepository(session)
     repository.add(user_request(username="test"))
     session.commit()
@@ -48,7 +48,7 @@ def test_getting_user_by_id_returns_user(session, user_request):
 
 
 @pytest.mark.integration
-def test_get_all_returns_all_users(session, user_request):
+def test_get_all_returns_all_users_in_db(session, user_request):
     repository = RelationalUserRepository(session)
     repository.add(user_request(username="test 1", email="a@b.com"))
     repository.add(user_request(username="test 2", email="b@a.com"))
@@ -59,14 +59,14 @@ def test_get_all_returns_all_users(session, user_request):
 
 
 @pytest.mark.integration
-def test_update_inexistent_user_raises_not_found_error(session, user_request):
+def test_update_user_not_in_db_raises_not_found_error(session, user_request):
     repository = RelationalUserRepository(session)
     with pytest.raises(NotFoundException):
         repository.update(id=1, entity=user_request())
 
 
 @pytest.mark.integration
-def test_update_user_updates_user(session, user_request):
+def test_update_user_in_db_updates_fields(session, user_request):
     repository = RelationalUserRepository(session)
     repository.add(user_request())
     session.commit()
@@ -81,14 +81,14 @@ def test_update_user_updates_user(session, user_request):
 
 
 @pytest.mark.integration
-def test_delete_inexistent_user_raises_not_found_error(session):
+def test_delete_user_not_in_db_raises_not_found_error(session):
     repository = RelationalUserRepository(session)
     with pytest.raises(NotFoundException):
         repository.delete(id=1)
 
 
 @pytest.mark.integration
-def test_delete_user_deletes_user(session, user_request):
+def test_delete_user_in_db_removes_it(session, user_request):
     repository = RelationalUserRepository(session)
     repository.add(user_request())
     session.commit()
