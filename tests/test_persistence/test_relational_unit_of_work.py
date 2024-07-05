25a5573b1f351405dfe4086e97ae3c87e26f7b5e
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-07-05 16:25:20 -0300

    Fix unit of work spelling in test

--- tests/test_persistence/test_relational_unit_of_work.py
+++ tests/test_persistence/test_relational_unit_of_work.py
@@ -6,10 +6,10 @@ from fast_api_tutorial.exceptions import DuplicateFieldError
 def test_relational_unit_of_work_raises_duplicate_error_on_commit_if_user_already_exists(
     session, user_request
 ):
-    wow = RelationalUnitOfWork(session_factory=lambda: session)
-    with wow:
-        wow.user_repository.add(user_request(username="test", email="a@b.com"))
-        wow.user_repository.add(user_request(username="test", email="c@d.com"))
+    uow = RelationalUnitOfWork(session_factory=lambda: session)
+    with uow:
+        uow.user_repository.add(user_request(username="test", email="a@b.com"))
+        uow.user_repository.add(user_request(username="test", email="c@d.com"))
         with pytest.raises(DuplicateFieldError) as e:
-            wow.commit()
+            uow.commit()
         assert "username" in str(e)
