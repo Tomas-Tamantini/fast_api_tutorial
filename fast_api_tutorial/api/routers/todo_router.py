7df26d69b82cd35c30010e139b280386e47587f1
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-07-17 12:44:13 -0300

    Start implementing create TODO endpoint

--- fast_api_tutorial/api/routers/todo_router.py
+++ fast_api_tutorial/api/routers/todo_router.py
@@ -1,5 +1,15 @@
 from http import HTTPStatus
 from fastapi import APIRouter
-
+from fast_api_tutorial.schemas import TodoRequest, TodoResponse
+from fast_api_tutorial.api.dependencies import T_UnitOfWork, T_CurrentUser
 
 todo_router = APIRouter(prefix="/todos", tags=["todos"])
+
+
+@todo_router.post("/", status_code=HTTPStatus.CREATED, response_model=TodoResponse)
+def create_todo(
+    todo: TodoRequest,
+    current_user: T_CurrentUser,
+    uow: T_UnitOfWork,
+):
+    raise NotImplementedError()
---
+++ tests/test_api/test_todo_endpoints.py
@@ -0,0 +1,25 @@
+from http import HTTPStatus
+from fast_api_tutorial.exceptions import BadTokenError
+
+
+def _make_create_request(client, request_body: dict, token="good_token"):
+    return client.post(
+        "/todos/", json=request_body, headers={"Authorization": f"Bearer {token}"}
+    )
+
+
+def test_create_todo_returns_unauthorized_if_no_token(client):
+    response = client.post("/todos/", json={}, headers={})
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+def test_create_todo_returns_unauthorized_if_bad_token(client, jwt_builder):
+    jwt_builder.get_token_subject.side_effect = BadTokenError
+    response = _make_create_request(client, request_body={}, token="bad_token")
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert jwt_builder.get_token_subject.call_args[0][0] == "bad_token"
+
+
+def test_create_invalid_todo_returns_unprocessable_entity(client):
+    response = _make_create_request(client, request_body={"bad": "body"})
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
