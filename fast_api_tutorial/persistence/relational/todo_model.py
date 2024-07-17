37e2e3bef4d07be294b4cde31dbfb89990affa16
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-07-17 13:39:16 -0300

    Relate user_id of todo to users table

--- fast_api_tutorial/persistence/relational/todo_model.py
+++ fast_api_tutorial/persistence/relational/todo_model.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import func
+from sqlalchemy import func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from fast_api_tutorial.persistence.relational.table_registry import table_registry
 from fast_api_tutorial.schemas import TodoStatus
@@ -13,4 +13,8 @@ class TodoDB:
     title: Mapped[str]
     description: Mapped[str]
     status: Mapped[TodoStatus]
-    user_id: Mapped[int]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
